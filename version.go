bc165a4d1c70c9eabfc5ad58093085c5514a3802
Gerard Forcada <gerardforcada@gmail.com>
2023-11-18 17:49:12 +0100

    feat: extract parsing version logic

--- version.go
+++ version.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/gerardforcada/structera/helpers"
 	"go/ast"
 	"reflect"
 	"strconv"
 	"strings"
+)
+
+const (
+	VersionTag = "version"
 )
 
 type Version struct {
@@ -18,19 +21,19 @@ func (v *Version) IdentifyVersions(structType *ast.StructType) {
 	// Collect all version tags from the struct fields
 	for _, field := range structType.Fields.List {
 		if field.Tag != nil {
-			tag := reflect.StructTag(field.Tag.Value[1 : len(field.Tag.Value)-1]).Get("version")
+			tag := reflect.StructTag(field.Tag.Value[1 : len(field.Tag.Value)-1]).Get(VersionTag)
 			allTags = append(allTags, tag)
 		}
 	}
 
-	maxVersion := determineMaxVersion(allTags)
+	maxVersion := v.determineMaxVersion(allTags)
 
 	versionMap := make(map[int][]string)
 	for _, field := range structType.Fields.List {
 		var versions []int
 		if field.Tag != nil {
-			tag := reflect.StructTag(field.Tag.Value[1 : len(field.Tag.Value)-1]).Get("version")
-			versions = parseVersionTag(tag, maxVersion)
+			tag := reflect.StructTag(field.Tag.Value[1 : len(field.Tag.Value)-1]).Get(VersionTag)
+			versions = v.parseVersionTag(tag, maxVersion)
 		} else {
 			for v := 1; v <= maxVersion; v++ {
 				versions = append(versions, v)
@@ -50,94 +53,85 @@ func (v *Version) IdentifyVersions(structType *ast.StructType) {
 	v.Versions = versionMap
 }
 
-func parseVersionTag(tag string, maxVersion int) []int {
-	var versions []int
-
-	if tag == "" {
-		return []int{1} // Default to version 1 if no tag
+func (v *Version) parseVersionTag(tag string, maxVersion int) []int {
+	start, end, err := v.parseVersionRange(tag)
+	if err != nil || start > maxVersion {
+		return []int{}
 	}
 
-	if strings.Contains(tag, "+") {
-		// For "2+" style tags
-		minVersion, err := strconv.Atoi(strings.TrimSuffix(tag, "+"))
-		if err != nil {
-			return []int{} // Error in parsing the tag
-		}
-		for v := minVersion; v <= maxVersion; v++ {
-			versions = append(versions, v)
-		}
-	} else if strings.Contains(tag, "-") {
-		// For "1-3" or "-3" style tags
-		parts := strings.Split(tag, "-")
-		var start, end int
-		var err error
-		if parts[0] == "" { // For "-3" style tags
-			start = 1
-			end, err = strconv.Atoi(parts[1])
-		} else { // For "1-3" style tags
-			start, err = strconv.Atoi(parts[0])
-			end, err = strconv.Atoi(parts[1])
-		}
-		if err != nil {
-			return []int{} // Error in parsing the tag
-		}
-		for v := start; v <= end; v++ {
-			versions = append(versions, v)
-		}
-	} else {
-		// For single version tags like "2"
-		version, err := strconv.Atoi(tag)
-		if err != nil {
-			return []int{} // Error in parsing the tag
-		}
-		versions = []int{version}
+	if end == -1 { // No upper limit specified
+		end = maxVersion
+	}
+
+	var versions []int
+	for v := start; v <= end; v++ {
+		versions = append(versions, v)
 	}
 
 	return versions
 }
 
-func determineMaxVersion(versionTags []string) int {
-	maxVersion := 1 // Default to version 1 if no higher versions are found
+func (v *Version) determineMaxVersion(versionTags []string) int {
+	maxVersion := 1
 
 	for _, tag := range versionTags {
-		if strings.Contains(tag, "+") {
-			// For "2+" style tags, assume a large number since we don't have the upper limit
-			parts := strings.Split(tag, "+")
-			value, err := strconv.Atoi(parts[0])
-			if err != nil {
-				continue // Error in parsing the tag
-			}
-			maxVersion = helpers.Max(maxVersion, value)
-		} else if strings.Contains(tag, "-") {
-			// For "1-3" or "-3" style tags
-			parts := strings.Split(tag, "-")
-			var end int
-			var err error
-			if parts[0] == "" { // For "-3" style tags
-				end, err = strconv.Atoi(parts[1])
-			} else { // For "1-3" style tags
-				end, err = strconv.Atoi(parts[1])
-			}
-			if err == nil {
-				maxVersion = helpers.Max(maxVersion, end)
-			}
-		} else {
-			// For single version tags like "2"
-			v, err := strconv.Atoi(tag)
-			if err == nil {
-				maxVersion = helpers.Max(maxVersion, v)
-			}
+		_, end, err := v.parseVersionRange(tag)
+		if err == nil && end > maxVersion {
+			maxVersion = end
 		}
 	}
 
 	return maxVersion
 }
 
+func (v *Version) parseVersionRange(tag string) (int, int, error) {
+	if tag == "" {
+		return 1, 1, nil // Default to version 1 if no tag
+	}
+
+	if strings.Contains(tag, "+") {
+		// For "2+" style tags
+		start, err := strconv.Atoi(strings.TrimSuffix(tag, "+"))
+		if err != nil {
+			return 0, 0, err // Error in parsing the tag
+		}
+		return start, -1, nil // -1 indicates no upper limit
+	}
+
+	if strings.Contains(tag, "-") {
+		// For "1-3" or "-3" style tags
+		parts := strings.Split(tag, "-")
+		start, end := 1, 0
+		var err error
+
+		if parts[0] != "" {
+			start, err = strconv.Atoi(parts[0])
+			if err != nil {
+				return 0, 0, err // Error in parsing the tag
+			}
+		}
+
+		end, err = strconv.Atoi(parts[1])
+		if err != nil {
+			return 0, 0, err // Error in parsing the tag
+		}
+
+		return start, end, nil
+	}
+
+	// For single version tags like "2"
+	version, err := strconv.Atoi(tag)
+	if err != nil {
+		return 0, 0, err // Error in parsing the tag
+	}
+	return version, version, nil
+}
+
 func (v *Version) ExcludeVersionTag(tag string) string {
 	var result []string
 	tags := strings.Split(tag, " ")
 	for _, t := range tags {
-		if !strings.HasPrefix(t, "version:") {
+		if !strings.HasPrefix(t, fmt.Sprintf("%s:", VersionTag)) {
 			result = append(result, t)
 		}
 	}
